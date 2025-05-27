d53ce9df3f76a02be2e54830ca3bf7405e8db2a6
raymondjxu <53128754+raymondjxu@users.noreply.github.com>
2025-05-27 08:33:10 -0400

    Deploy files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Causality
-
-A timeline reviewing tool.
--- src/orderedEventList.js
+++
@@ -1,8 +0,0 @@
-// List of events in the correct order for the timeline
-const orderedEventList = [
-  { label: 'Event 1' },
-  { label: 'Event 2' },
-  { label: 'Event 3' },
-];
-
-export default orderedEventList;
